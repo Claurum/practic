09cb7e94e67a5a02a1020776039b56d7ab39c472
Claurum <150020958+Claurum@users.noreply.github.com>
2026-05-12 11:39:16 +0300

    Add files via upload

--- praktikapreddiplom-main/src/app/modules/administration/administration-routing.ts
+++ praktikapreddiplom-main/src/app/modules/administration/administration-routing.ts
@@ -1,26 +1,21 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { AdminPageComponent } from "./pages/admin-page/admin-page.component";
-import { UsersPageComponent } from "./pages/users-page/users-page.component";
-import { AdminsPageComponent } from "./pages/admins-page/admins-page.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { UsersPageComponent } from './pages/users-page/users-page.component';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: AdminPageComponent,
   },
   {
-    path: "users",
-    component: UsersPageComponent,
-  },
-  {
-    path: "admins",
-    component: AdminsPageComponent,
-  },
+    path: 'users',
+    component: UsersPageComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AdministrationRoutingModule {}
+export class AdministrationRoutingModule { }
